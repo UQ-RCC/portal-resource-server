bffb8d69dcbd8bc71cf3a4b84238715a1acf34f7
Zane van Iperen <z.vaniperen@uq.edu.au>
2019-11-28 14:38:19 +1000

    cleanup certificate management code

---
+++ src/main/java/au/org/massive/strudel_web/ssh/CertFiles.java
@@ -0,0 +1,102 @@
+package au.org.massive.strudel_web.ssh;
+
+import au.org.massive.strudel_web.util.UnsupportedKeyException;
+import au.org.rcc.miscs.ResourceServerSettings;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CertFiles implements Closeable {
+	private final Path tempDirectory;
+	private final Path privKeyFile;
+	private final Path certFile;
+	private final Path tempPrivKeyFile;
+
+	public CertFiles(CertAuthInfo authInfo) throws IOException, UnsupportedKeyException {
+		Path defaultPath = FileSystems.getDefault().getPath(ResourceServerSettings.getInstance().getTempDir());
+		tempDirectory = Files.createTempDirectory(defaultPath, "coesra-" + authInfo.getUserName());
+		privKeyFile = tempDirectory.resolve("id_rsa");
+		certFile = tempDirectory.resolve("id_rsa-cert.pub");
+		tempPrivKeyFile = tempDirectory.resolve("id_rsa-cert");
+
+		Set<PosixFilePermission> perms = new HashSet<>();
+		perms.add(PosixFilePermission.OWNER_READ);
+
+		Set<OpenOption> opts = new HashSet<>();
+		opts.add(StandardOpenOption.CREATE);
+		opts.add(StandardOpenOption.TRUNCATE_EXISTING);
+		opts.add(StandardOpenOption.WRITE);
+
+		ByteBuffer bb = ByteBuffer.wrap(authInfo.getPrivateKey().getBytes(StandardCharsets.UTF_8));
+
+		/* NB: newByteChannel() is the only way to do this atomically. */
+		try(ByteChannel c = Files.newByteChannel(privKeyFile, opts, PosixFilePermissions.asFileAttribute(perms))) {
+			c.write(bb);
+		}
+
+		try(ByteChannel c = Files.newByteChannel(tempPrivKeyFile, opts, PosixFilePermissions.asFileAttribute(perms))) {
+			c.write(bb);
+		}
+
+		Files.write(certFile, authInfo.getCertificate().getBytes(StandardCharsets.UTF_8));
+	}
+
+	public Path getPrivKeyFile() {
+		return privKeyFile;
+	}
+
+	@SuppressWarnings("unused")
+	public Path getCertFile() {
+		return certFile;
+	}
+
+	@Override
+	public void close() throws IOException {
+		IOException ex = new IOException("Unable to delete certificate files");
+
+		boolean failed = false;
+		try {
+			Files.deleteIfExists(tempPrivKeyFile);
+		} catch(IOException e) {
+			ex.addSuppressed(e);
+			failed = true;
+		}
+
+		try {
+			Files.deleteIfExists(privKeyFile);
+		} catch(IOException e) {
+			ex.addSuppressed(e);
+			failed = true;
+		}
+
+		try {
+			Files.deleteIfExists(certFile);
+		} catch(IOException e) {
+			ex.addSuppressed(e);
+			failed = true;
+		}
+
+		try {
+			Files.deleteIfExists(tempDirectory);
+		} catch(IOException e) {
+			ex.addSuppressed(e);
+			failed = true;
+		}
+
+		if(failed) {
+			throw ex;
+		}
+	}
+}
--- src/main/java/au/org/massive/strudel_web/ssh/ForkedSSHClient.java
+++ src/main/java/au/org/massive/strudel_web/ssh/ForkedSSHClient.java
@@ -1,27 +1,7 @@
 package au.org.massive.strudel_web.ssh;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.Closeable;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermission;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Callable;
-
+import au.org.massive.strudel_web.util.UnsupportedKeyException;
+import au.org.rcc.miscs.ResourceServerSettings;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.apache.commons.exec.CommandLine;
@@ -32,9 +12,17 @@ import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import au.org.massive.strudel_web.util.UnsupportedKeyException;
-import au.org.rcc.miscs.ResourceServerSettings;
-import org.codehaus.jackson.annotate.JsonManagedReference;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
 
 /**
  * A non-native SSH client implmentation that forks SSH processes for each request.
@@ -56,62 +44,6 @@ public class ForkedSSHClient extends AbstractSSHClient {
     public ForkedSSHClient(CertAuthInfo authInfo, String viaGateway, String remoteHost) {
         super(authInfo, viaGateway, remoteHost);
         this.authInfo = authInfo;
-    }
-
-    private class CertFiles implements Closeable {
-        private final File tempDirectory;
-        private final File privKeyFile;
-        private final File certFile;
-        private final File tempPrivKeyFile;
-        
-        public CertFiles(CertAuthInfo authInfo) throws IOException, UnsupportedKeyException {
-            super();
-            Path defaultPath = FileSystems.getDefault().getPath(ResourceServerSettings.getInstance().getTempDir());
-            Path tempDirectoryPath = 
-            Files.createTempDirectory(defaultPath, "coesra-" + authInfo.getUserName());
-            privKeyFile = tempDirectoryPath.resolve("id_rsa").toFile();
-            privKeyFile.createNewFile();
-            
-            certFile = tempDirectoryPath.resolve("id_rsa-cert.pub").toFile();
-            tempDirectory = tempDirectoryPath.toFile();
-            certFile.createNewFile();
-
-            PrintWriter out = new PrintWriter(privKeyFile);
-            out.println(authInfo.getPrivateKey());
-            out.close();
-            
-            //create a new temporary file for later version of ssh
-            tempPrivKeyFile = tempDirectoryPath.resolve("id_rsa-cert").toFile();
-            out = new PrintWriter(tempPrivKeyFile);
-            out.println(authInfo.getPrivateKey());
-            out.close();
-
-            out = new PrintWriter(certFile);
-            out.println(authInfo.getCertificate());
-            out.close();
-
-            Set<PosixFilePermission> perms = new HashSet<>();
-            perms.add(PosixFilePermission.OWNER_READ);
-            Files.setPosixFilePermissions(privKeyFile.toPath(), perms);
-            Files.setPosixFilePermissions(tempPrivKeyFile.toPath(), perms);
-        }
-
-        public File getPrivKeyFile() {
-            return privKeyFile;
-        }
-
-        @SuppressWarnings("unused")
-        public File getCertFile() {
-            return certFile;
-        }
-
-        @Override
-        public void close() throws IOException {
-            tempPrivKeyFile.delete();
-            privKeyFile.delete();
-            certFile.delete();
-            tempDirectory.delete();
-        }
     }
 
     private int findFreePort() throws IOException {
@@ -225,7 +157,7 @@ public class ForkedSSHClient extends AbstractSSHClient {
         //cmdLine.addArgument("-q");
         cmdLine.addArgument("-q");
         cmdLine.addArgument("-i");
-        cmdLine.addArgument(certFiles.getPrivKeyFile().getAbsolutePath());
+        cmdLine.addArgument(certFiles.getPrivKeyFile().toAbsolutePath().toString());
         cmdLine.addArgument("-oUserKnownHostsFile=/dev/null");
         cmdLine.addArgument("-oStrictHostKeyChecking=no"); // TODO: Remove me when ready
         cmdLine.addArgument("-oBatchMode=yes");
