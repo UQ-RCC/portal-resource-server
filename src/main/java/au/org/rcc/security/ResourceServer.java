47615e2426b26aac83124e346666536faf2caebb
Zane van Iperen <z.vaniperen@uq.edu.au>
2020-01-10 13:56:37 +1000

    add audience validation back

--- src/main/java/au/org/rcc/security/ResourceServer.java
+++ src/main/java/au/org/rcc/security/ResourceServer.java
@@ -1,15 +1,31 @@
 package au.org.rcc.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.IssuerUriCondition;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 
@@ -41,6 +57,55 @@ public class ResourceServer extends WebSecurityConfigurerAdapter {
 				.oauth2ResourceServer()
 				.jwt()
 				.jwtAuthenticationConverter(jc);
+	}
+
+	/* All of this is edited from OAuth2ResourceServerJwtConfiguration */
+
+	@Value("${spring.security.oauth2.resourceserver.jwt.audience-id}")
+	private String audience;
+
+	@Bean
+	@ConditionalOnProperty(name = "spring.security.oauth2.resourceserver.jwt.jwk-set-uri")
+	JwtDecoder jwtDecoderByJwkKeySetUri(OAuth2ResourceServerProperties properties) {
+		OAuth2ResourceServerProperties.Jwt jwtProperties = properties.getJwt();
+
+		NimbusJwtDecoder nimbusJwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwtProperties.getJwkSetUri())
+				.jwsAlgorithm(SignatureAlgorithm.from(jwtProperties.getJwsAlgorithm())).build();
+
+		List<OAuth2TokenValidator<Jwt>> validators = new ArrayList<>();
+		String issuerUri = jwtProperties.getIssuerUri();
+		if(issuerUri != null) {
+			validators.add(JwtValidators.createDefaultWithIssuer(issuerUri));
+		}
+
+		if(audience != null) {
+			final OAuth2Error error = new OAuth2Error("access_denied", String.format(
+					"Invalid token does not contain resource id (%s)", audience
+			), null);
+
+			validators.add(jwt -> {
+				if(!jwt.getAudience().contains(audience)) {
+					return OAuth2TokenValidatorResult.failure(error);
+				}
+				return OAuth2TokenValidatorResult.success();
+			});
+		}
+
+		nimbusJwtDecoder.setJwtValidator(new DelegatingOAuth2TokenValidator<>(validators));
+
+		return nimbusJwtDecoder;
+	}
+
+	@Bean
+	@Conditional(IssuerUriCondition.class)
+	JwtDecoder jwtDecoderByIssuerUri(OAuth2ResourceServerProperties properties) {
+		/*
+		 * Deliberately stopping this:
+		 * - I don't want the auth server being down to stop this from starting
+		 * - There's no way to add the audience validator in here
+		 *   (as JwtDecoderProviderConfigurationUtils) is package private.
+		 */
+		throw new UnsupportedOperationException("Refusing to allow start without spring.security.oauth2.resourceserver.jwt.jwk-set-uri");
 	}
 }
 
