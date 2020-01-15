60da62528b9dd0e62fbcba8cecf4e1c4af19f28d
Zane van Iperen <z.vaniperen@uq.edu.au>
2020-01-15 18:10:59 +1000

    remove nimrod-specific endpoints

--- src/main/java/au/edu/uq/rcc/portal/resource/controller/Config.java
+++ src/main/java/au/edu/uq/rcc/portal/resource/controller/Config.java
@@ -24,17 +24,11 @@ import au.org.massive.strudel_web.job_control.ConfigurationRegistry;
 import au.org.massive.strudel_web.job_control.InvalidJsonConfigurationException;
 import au.org.massive.strudel_web.job_control.StrudelDesktopConfigurationAdapter;
 import au.org.massive.strudel_web.vnc.GuacamoleSessionManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -42,18 +36,6 @@ import java.nio.file.Paths;
 
 @Configuration
 public class Config {
-	@Bean(name = "nimrodDataSource")
-	@ConfigurationProperties(prefix = "nimrod.datasource")
-	public DataSource nimrodDataSource() {
-		return DataSourceBuilder.create().build();
-	}
-
-	@Bean(name = "nimrodDb")
-	@Autowired
-	public JdbcTemplate nimrodJdbcTemplate(@Qualifier("nimrodDataSource") DataSource ds) {
-		return new JdbcTemplate(ds);
-	}
-
 	@Bean
 	public GuacamoleSessionManager guacamoleSessionManager(ResourceServerSettings settings, CertAuthManager certAuthManager, ThreadPoolTaskExecutor taskExecutor) {
 		return new GuacamoleSessionManager(settings.getTmpDir(), certAuthManager, taskExecutor.getThreadPoolExecutor());
